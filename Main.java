9da7efe67d851c3745ef62c858040e5b105101a2
Daniel-Chacha <danmwita355@gmail.com>
2025-02-13 11:39:47 +0300

    first java program

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Welcome to Java!");
+        System.out.println("5+3");
+        System.out.println(5+3);
+        System.out.println("Sum of 3 & 4 is " + (3+4));
+    }
+} 
